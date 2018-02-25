ac28fa67bbc597b136679a8a962d24b4a92056ee
tu_linux2 <tu_linux2@localhost.localdomain>
2018-02-25 15:27:23 +0900

    add makeDif.py

--- GAN/dcgan/dcgan.py
+++ GAN/dcgan/dcgan.py
@@ -15,10 +15,10 @@ import sys
 
 import numpy as np
 
-class GAN():
-
+class DCGAN():
     
     def __init__(self):
+        self.path = "/volumes/data/dataset/gan/MNIST/dcgan/dcgan_generated_images/"
         #mnistデータ用の入力データサイズ
         self.img_rows = 28 
         self.img_cols = 28
@@ -31,11 +31,20 @@ class GAN():
         # 画像保存の際の列、行数
         self.row = 5
         self.col = 5
+        self.row2 = 1 # 連続潜在変数用
+        self.col2 = 10# 連続潜在変数用 
         
         # 画像生成用の固定された入力潜在変数
-        self.noise_fix1 = np.random.normal(0, 1, (self.row * self.col, self.z_dim))
+        self.noise_fix1 = np.random.normal(0, 1, (self.row * self.col, self.z_dim)) 
+        # 連続的に潜在変数を変化させる際の開始、終了変数
         self.noise_fix2 = np.random.normal(0, 1, (1, self.z_dim))
         self.noise_fix3 = np.random.normal(0, 1, (1, self.z_dim))
+
+        # 横軸がiteration数のプロット保存用np.ndarray
+        self.g_loss_array = np.array([])
+        self.d_loss_array = np.array([])
+        self.d_accuracy_array = np.array([])
+        self.d_predict_true_num_array = np.array([])
 
         discriminator_optimizer = Adam(lr=1e-5, beta_1=0.1)
         combined_optimizer = Adam(lr=2e-4, beta_1=0.5)
@@ -121,6 +130,11 @@ class GAN():
 
         half_batch = int(batch_size / 2)
 
+        self.g_loss_array = np.zeros(epochs)
+        self.d_loss_array = np.zeros(epochs)
+        self.d_accuracy_array = np.zeros(epochs)
+        self.d_predict_true_num_array = np.zeros(epochs)
+
         for epoch in range(epochs):
 
             # ---------------------
@@ -143,6 +157,10 @@ class GAN():
             # それぞれの損失関数を平均
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
+            # discriminatorの予測（本物と偽物が半々のミニバッチ）
+            d_predict = self.discriminator.predict_classes(np.concatenate([gen_imgs,imgs]), verbose=0)
+            d_predict = np.sum(d_predict)
+
 
             # ---------------------
             #  Generatorの学習
@@ -150,55 +168,91 @@ class GAN():
 
             noise = np.random.normal(0, 1, (batch_size, self.z_dim))
 
+
             # 生成データの正解ラベルは本物（1） 
             valid_y = np.array([1] * batch_size)
 
             # Train the generator
             g_loss = self.combined.train_on_batch(noise, valid_y)
 
+
             # 進捗の表示
             print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
-            # 指定した間隔で生成画像を保存
+            # np.ndarrayにloss関数を格納
+            self.g_loss_array[epoch] = g_loss
+            self.d_loss_array[epoch] = d_loss[0]
+            self.d_accuracy_array[epoch] = 100*d_loss[1]
+            self.d_predict_true_num_array[epoch] = d_predict
+
             if epoch % save_interval == 0:
-                noise = np.random.normal(0, 1, (self.row * self.col, self.z_dim))
+                
                 # 毎回異なる乱数から画像を生成
-                self.save_imgs(epoch, '', noise)
+                self.save_imgs(self.row, self.col, epoch, '', noise)
                 # 毎回同じ値から画像を生成
-                self.save_imgs(epoch, 'fromFixedValue', self.noise_fix1)
+                self.save_imgs(self.row, self.col, epoch, 'fromFixedValue', self.noise_fix1)
                 # 二つの潜在変数の間の遷移画像を生成
                 total_images = self.row*self.col
                 noise_trans = np.zeros((total_images, self.z_dim))
                 for i in range(total_images):
-                    t = (i*1.)/(total_images*1.)
+                    t = (i*1.)/((total_images-1)*1.)
                     noise_trans[i,:] = t * self.noise_fix2 + (1-t) * self.noise_fix3
-                self.save_imgs(epoch, 'trans', noise_trans)
+                self.save_imgs(self.row2, self.col2, epoch, 'trans', noise_trans)
+       
 
-                
+                # 重みを保存
+                self.generator.save_weights(self.path + "generator_%s.h5" % epoch)
+                self.discriminator.save_weights(self.path + "discriminator_%s.h5" % epoch)
 
-    def save_imgs(self, epoch, filename, noise):
+                # 学習結果をプロット
+                fig, ax = plt.subplots(4,1, figsize=(8.27,11.69))
+                ax[0].plot(self.g_loss_array[:epoch])
+                ax[0].set_title("g_loss")
+                ax[1].plot(self.d_loss_array[:epoch])
+                ax[1].set_title("d_loss")
+                ax[2].plot(self.d_accuracy_array[:epoch])
+                ax[2].set_title("d_accuracy")
+                ax[3].plot(self.d_predict_true_num_array[:epoch])
+                ax[3].set_title("d_predict_true_num_array")
+                fig.suptitle("epoch: %5d" % epoch)
+                fig.savefig(self.path + "training_%d.png" % epoch)
+                plt.close()
+
+            
+
+    def save_imgs(self, row, col, epoch, filename, noise):
         # row, col
         # 生成画像を敷き詰めるときの行数、列数
-
+    
         gen_imgs = self.generator.predict(noise)
-
+    
         # 生成画像を0-1に再スケール
         gen_imgs = 0.5 * gen_imgs + 0.5
-
-        fig, axs = plt.subplots(self.row, self.col)
+    
+    
+        fig, axs = plt.subplots(row, col)
         cnt = 0
-        for i in range(self.row):
-            for j in range(self.col):
-                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
-                axs[i,j].axis('off')
+        if row == 1:
+            for j in range(col):
+                axs[j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[j].axis('off')
                 cnt += 1
-        fig.savefig("images/mnist_%s_%d.png" % (filename, epoch))
+        else:
+            for i in range(row):
+                for j in range(col):
+                    axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                    axs[i,j].axis('off')
+                    cnt += 1
+
+        #fig.savefig("images/mnist_%s_%d.png" % (filename, epoch))
+        fig.suptitle("epoch: %5d" % epoch)
+        fig.savefig(self.path + "mnist_%s_%d.png" % (filename, epoch))
         plt.close()
-
-
+    
+ 
 if __name__ == '__main__':
-    gan = GAN()
-    gan.train(epochs=30000, batch_size=32, save_interval=500)
+    gan = DCGAN()
+    gan.train(epochs=30000, batch_size=32, save_interval=1000)
 
 
 
