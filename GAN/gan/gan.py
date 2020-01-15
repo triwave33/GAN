68ef95f709ef3a2575b083be7ab1f504ecbf0cdf
triwave33 <triwave33@gmail.com>
2020-01-15 12:24:28 +0900

    change comment in English

--- GAN/gan/gan.py
+++ GAN/gan/gan.py
@@ -17,31 +17,34 @@ import numpy as np
 
 class GAN():
     def __init__(self):
-        #mnistデータ用の入力データサイズ
+        # datasize for mnist image set
         self.img_rows = 28 
         self.img_cols = 28
         self.channels = 1
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
         
-        # 潜在変数の次元数 
         self.z_dim = 100
 
         optimizer = Adam(0.0002, 0.5)
 
-        # discriminatorモデル
+        # discriminator model
         self.discriminator = self.build_discriminator()
         self.discriminator.compile(loss='binary_crossentropy', 
             optimizer=optimizer,
             metrics=['accuracy'])
 
-        # Generatorモデル
+        # Generator model
         self.generator = self.build_generator()
-        # generatorは単体で学習しないのでコンパイルは必要ない
+        # we don't need to compile generator because generator learns with descriminatorgenerator as 'combined'
         #self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
-
+        
+        # we can define combined model by two ways (results are same)
+        # please see https://qiita.com/triwave33/items/1890ccc71fab6cbca87e
+        # sorry in Japanese
         self.combined = self.build_combined1()
         #self.combined = self.build_combined2()
-        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+        self.combined.compile(loss='binary_crossentropy', 
+                                    optimizer=optimizer)
 
     def build_generator(self):
 
@@ -98,10 +101,9 @@ class GAN():
 
     def train(self, epochs, batch_size=128, save_interval=50):
 
-        # mnistデータの読み込み
         (X_train, _), (_, _) = mnist.load_data()
 
-        # 値を-1 to 1に規格化
+        # normalize
         X_train = (X_train.astype(np.float32) - 127.5) / 127.5
         X_train = np.expand_dims(X_train, axis=3)
 
@@ -115,53 +117,53 @@ class GAN():
             for iteration in range(num_batches):
 
                 # ---------------------
-                #  Discriminatorの学習
+                #  Discriminator learning
                 # ---------------------
 
-                # バッチサイズの半数をGeneratorから生成
+                # pickup images (half-batch size) from generator 
                 noise = np.random.normal(0, 1, (half_batch, self.z_dim))
                 gen_imgs = self.generator.predict(noise)
 
 
-                # バッチサイズの半数を教師データからピックアップ
+                # pickup images (half-batch size) from dataset
                 idx = np.random.randint(0, X_train.shape[0], half_batch)
                 imgs = X_train[idx]
 
-                # discriminatorを学習
-                # 本物データと偽物データは別々に学習させる
+                # learn discriminator
+                # learning discriminator with real-data and fake-data seperately
                 d_loss_real = self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
                 d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
-                # それぞれの損失関数を平均
+                # average each loss
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
 
                 # ---------------------
-                #  Generatorの学習
+                #  Generator learning
                 # ---------------------
 
                 noise = np.random.normal(0, 1, (batch_size, self.z_dim))
 
-                # 生成データの正解ラベルは本物（1） 
+                # make label (gen data: 1) 
                 valid_y = np.array([1] * batch_size)
 
                 # Train the generator
                 g_loss = self.combined.train_on_batch(noise, valid_y)
 
-                # 進捗の表示
+                # progress
                 print ("epoch:%d, iter:%d,  [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, iteration, d_loss[0], 100*d_loss[1], g_loss))
 
-                # 指定した間隔で生成画像を保存
+                # save images 
                 if epoch % save_interval == 0:
                     self.save_imgs(epoch)
 
     def save_imgs(self, epoch):
-        # 生成画像を敷き詰めるときの行数、列数
+        # row,col
         r, c = 5, 5
 
         noise = np.random.normal(0, 1, (r * c, self.z_dim))
         gen_imgs = self.generator.predict(noise)
 
-        # 生成画像を0-1に再スケール
+        # rescale [-1, 1] to [0, 1]
         gen_imgs = 0.5 * gen_imgs + 0.5
 
         fig, axs = plt.subplots(r, c)
